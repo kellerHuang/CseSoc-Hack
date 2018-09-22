b86af9932c60fdce3e72dd4900dc33c3a4c0e2f6
Evan154 <gabokazu154@gmail.com>
2018-09-22 13:07:20 +1000

    timer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Countdown from './Countdown.js';
+var ProgressBar = require('react-progressbar.js');
+var Line = ProgressBar.Line
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+
+        /*const options = {
+            strokeWidth: 10
+        };
+
+        // For demo purposes so the container has some dimensions.
+        // Otherwise progress bar won't be shown
+        const containerStyle = {
+            width: '200px',
+            height: '200px'
+        };*/
+
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h2>racer</h2>
+                </div>
+
+                {/* <Line
+                    progress={0.1}
+                    text={'Progress'}
+                    options={options}
+                    initialAnimate={true}
+                    containerStyle={containerStyle}
+                    containerClassName={'.progressbar'} />*/}
+
+
+                <h3 className="title">Time till due:</h3>
+                <Countdown date={`2018-09-23T00:00:00`} />
+            </div>
+        );
+    }
 }
 
 export default App;
